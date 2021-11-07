70086dffd83bdca91988ed909ceb10d232873186
Andres Liu <andres.liu22@gmail.com>
2021-11-06 22:45:24 -0700

    Fix frontend repo api bugs

--- controllers/api/repoRoutes.js
+++ controllers/api/repoRoutes.js
@@ -5,7 +5,7 @@ router.post('/', async (req, res) => {
     try {
         const repoData = await Repo.create(req.body);
         !repoData ? res.status(403).json(new Error("No repositories found")) : null;
-        res.status(200).json(userData);
+        res.status(200).json(repoData);
       } catch (err) {
         res.status(500).json(err);
       }
--- public/js/repo.js
+++ public/js/repo.js
@@ -1,5 +1,3 @@
-const { Repo } = require("../../models");
-
 const getRepos = async () => {
     var apiUrl = 'https://api.github.com/users/andresliu22/repos?type=public&sort=created';
     
@@ -12,25 +10,24 @@ const getRepos = async () => {
 
     if (dataResponse.ok) {
         const repos = await dataResponse.json();
-        console.log(repos);
     for (let i = 0 ; i < repos.length ; i++) {
         if (repos[i].stargazers_count > 0) {
-            
-            let name = repos[i].name.replaceAll("-", " ");
-            let url_name = repos[i].name;
-            let url_github = repos[i].html_url;
-            let url_image =`https://raw.githubusercontent.com/andresliu22/${repos[i].name}/main/assets/images/site-img.PNG`;
-            let url_deploy_link =`https://andresliu22.github.io/${repos[i].name}`;
+            const name = repos[i].name.replaceAll("-", " ");
+            const url_name = repos[i].name;
+            const url_github = repos[i].html_url;
+            const url_image =`https://raw.githubusercontent.com/andresliu22/${repos[i].name}/main/assets/images/site-img.PNG`;
+            const url_deploy_link =`https://andresliu22.github.io/${repos[i].name}`;
 
             const response = await fetch(`/api/repos`, {
                 method: 'POST',
-                body: JSON.stringify({ name, url_name, url_github, url_image, url_deploy_link })
-            })
+                body: JSON.stringify({ name, url_name, url_github, url_image, url_deploy_link }),
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
 
-            if (response.ok) {
-                document.location.reload();
-            } else {
-                alert('Failed to load repos');
+            if (!response.ok) {
+                console.log('Failed to load repo');
             }
         }
     }
