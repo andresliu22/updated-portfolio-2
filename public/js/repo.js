9604ad891b54b805d24507cc5780f5491f8a67b7
Andres Liu <andres.liu22@gmail.com>
2021-11-06 21:34:50 -0700

    Set server.js

---
+++ public/js/repo.js
@@ -0,0 +1,71 @@
+let workRepos = $('#work-repos');
+let workSkills = $('#work-skills');
+
+
+$(document).ready(function (){
+    getRepos();
+    displaySkills();
+});
+
+function getRepos() {
+    var apiUrl = 'https://api.github.com/users/andresliu22/repos?type=public&sort=created';
+  
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+              console.log(data);
+            displayFeaturedRepos(data);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to GitHub');
+      });
+  };
+
+ 
+function displayFeaturedRepos(repos) {
+    for (let i = 0 ; i < repos.length ; i++) {
+        if (repos[i].stargazers_count > 0) {
+            let projectDiv = $('<div class="project">');
+            let header = $('<header>');
+            let repoName = repos[i].name.replaceAll("-", " ");
+
+            let aRepo = $('<a target="_blank">');
+            aRepo.attr("href", repos[i].html_url);
+            aRepo.text(repoName[0].toUpperCase() + repoName.slice(1));
+            
+            let aLive = $('<a target="_blank">');
+            aLive.attr("href",`https://andresliu22.github.io/${repos[i].name}`);
+
+            let imageDiv = $('<div class="image-div">');
+            let img = $('<img>');
+            img.attr("src", `https://raw.githubusercontent.com/andresliu22/${repos[i].name}/main/assets/images/site-img.PNG`);
+            img.attr("alt", `${repos[i].name}`);
+
+            projectDiv.append(header);
+            header.append(aRepo);
+            projectDiv.append(aLive);
+            aLive.append(imageDiv);
+            imageDiv.append(img);
+
+            workRepos.append(projectDiv);
+        }
+    }
+}
+
+function displaySkills() {
+    let skills = ["html", "css", "javascript", "angular", "react", "xcode", "swift", "sql"];
+
+    skills.forEach(skill => {
+        let skillDiv = $('<div class="skill">');
+        let img = $('<img>');
+        img.attr("src", `./assets/images/${skill}-logo.png`);
+        img.attr("alt", `${skill}-logo`);
+
+        workSkills.append(skillDiv);
+        skillDiv.append(img);
+    });
+}
--- server.js
+++ server.js
@@ -1,71 +1,39 @@
-let workRepos = $('#work-repos');
-let workSkills = $('#work-skills');
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-$(document).ready(function (){
-    getRepos();
-    displaySkills();
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'sus',
+    cookie: {},
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(()=> {
+    app.listen(PORT, () => console.log('Now listening'));
 });
-
-function getRepos() {
-    var apiUrl = 'https://api.github.com/users/andresliu22/repos?type=public&sort=created';
-  
-    fetch(apiUrl).then(function (response) {
-        if (response.ok) {
-          response.json().then(function (data) {
-              console.log(data);
-            displayFeaturedRepos(data);
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      })
-      .catch(function (error) {
-        alert('Unable to connect to GitHub');
-      });
-  };
-
- 
-function displayFeaturedRepos(repos) {
-    for (let i = 0 ; i < repos.length ; i++) {
-        if (repos[i].stargazers_count > 0) {
-            let projectDiv = $('<div class="project">');
-            let header = $('<header>');
-            let repoName = repos[i].name.replaceAll("-", " ");
-
-            let aRepo = $('<a target="_blank">');
-            aRepo.attr("href", repos[i].html_url);
-            aRepo.text(repoName[0].toUpperCase() + repoName.slice(1));
-            
-            let aLive = $('<a target="_blank">');
-            aLive.attr("href",`https://andresliu22.github.io/${repos[i].name}`);
-
-            let imageDiv = $('<div class="image-div">');
-            let img = $('<img>');
-            img.attr("src", `https://raw.githubusercontent.com/andresliu22/${repos[i].name}/main/assets/images/site-img.PNG`);
-            img.attr("alt", `${repos[i].name}`);
-
-            projectDiv.append(header);
-            header.append(aRepo);
-            projectDiv.append(aLive);
-            aLive.append(imageDiv);
-            imageDiv.append(img);
-
-            workRepos.append(projectDiv);
-        }
-    }
-}
-
-function displaySkills() {
-    let skills = ["html", "css", "javascript", "angular", "react", "xcode", "swift", "sql"];
-
-    skills.forEach(skill => {
-        let skillDiv = $('<div class="skill">');
-        let img = $('<img>');
-        img.attr("src", `./assets/images/${skill}-logo.png`);
-        img.attr("alt", `${skill}-logo`);
-
-        workSkills.append(skillDiv);
-        skillDiv.append(img);
-    });
-}
