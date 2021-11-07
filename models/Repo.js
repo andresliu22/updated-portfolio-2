0e5eb56eae07b6074d4f1f524bd5bd1091c4029c
Andres Liu <andres.liu22@gmail.com>
2021-11-06 21:31:34 -0700

    Create model

--- models/Repo.js
+++ models/Repo.js
@@ -1,1 +1,42 @@
-const { Model, DataTypes, Sequelize } = require('sequelize');
+const { timeStamp } = require('console');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Repo extends Model {};
+
+Repo.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        url_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        url_link: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        url_image: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'repo',
+    }
+
+);
+
+module.exports = Repo
